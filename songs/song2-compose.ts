9cd2e2a0e5e9f0b5bdb235cd42f65a1287f83488
Christopher Jacobs <apexearth@gmail.com>
2019-05-18 17:21:54 -0700

    Couch experimentation...

--- songs/song2-compose.ts
+++ songs/song2-compose.ts
@@ -4,25 +4,37 @@ import Song from "../src/core/Song";
 
 const song = new Song();
 const player = new TonePlayer(song)
-const track = song.createTrack()
+const track1 = song.createTrack()
+const track2 = song.createTrack()
 
-const composer = new Composer();
-composer.speed = .25;
-composer.clear()
-composer.array([
-    1, 1, 2, -5,
-    1, 1, 2, -5,
-    1, 1, 2, -5,
-    1, 2, 3, -3,
-    1, 1, 2, -5,
-    1, 1, 2, -5,
-    1, 1, 2, -5,
-    1, 2, 3, -3,
+const composer1 = new Composer();
+composer1.speed = .5;
+composer1.velocity *= .8;
+composer1.clear()
+composer1.array([
+    5, -7, 3, 3,
+    1, -7, 3, 3,
+    1, -7, 3, 3,
+    1, -7, 6, -3,
+
+    -1, -7, 3, 3,
+    1, -7, 3, 3,
+    1, -7, 3, 3,
+    1, -7, 3, 3,
 ])
 
-track.addNotes(1, composer.notes)
+const composer2 = new Composer({
+    note: 'C2',
+});
+composer2.speed = 2;
+composer2.clear()
+composer2.array([
+    0, 2, 1, 1,
+    -5, 2, 1, 1,
+])
 
-console.log(track.sections)
+track1.addNotes(1, composer1.notes)
+track2.addNotes(1, composer2.notes)
 
-player.loop(1, 32)
+player.loop(1, 16)
 player.start()
--- src/compose/Composer.ts
+++ src/compose/Composer.ts
@@ -3,24 +3,37 @@ import * as Key from "tonal-key";
 import Note from "../core/Note";
 
 export class Composer {
+    public key: string = 'C Major';
+    public note: string = 'C3';
+    public speed: number = 1;
+    public notes: Note[] = [];
+    public velocity: number = 100;
+
     public index: number = 1;
+    public startNote: string;
     public keyNotes: string[];
     public keyIndex: number = 0;
     public keyOctave: number = 3;
 
-    constructor(
-        public key: string = 'C Major',
-        public note: string = 'C3',
-        public speed: number = 1,
-        public notes: Note[] = [],
-        public velocity: number = 100,
-    ) {
-        this.keyNotes = Key.scale(key)
-        const startNote = /([A-G][#b]?)/.exec(note);
+    constructor(options?: {
+        key?: string,
+        note?: string,
+        speed?: number,
+        velocity?: number,
+    }) {
+        if (options) {
+            this.key = options.key || this.key;
+            this.note = options.note || this.note;
+            this.speed = options.speed || this.speed;
+            this.velocity = options.velocity || this.velocity;
+        }
+        this.startNote = this.note;
+        this.keyNotes = Key.scale(this.key)
+        const startNote = /([A-G][#b]?)/.exec(this.note);
         if (!startNote) {
             throw new Error('Invalid start note received.');
         }
-        const startOctave = /[0-9]/.exec(note);
+        const startOctave = /[0-9]/.exec(this.note);
         if (!startOctave) {
             throw new Error('Invalid start octave received.');
         }
@@ -41,6 +54,7 @@ export class Composer {
     }
 
     public clear() {
+        this.note = this.startNote;
         this.notes = []
     }
 
@@ -57,14 +71,16 @@ export class Composer {
         return this;
     }
 
-    public next(direction: number) {
-        this.moveNote(direction);
-        this.notes.push(Note.create({
-            note: this.note,
-            index: this.index,
-            duration: this.speed,
-            velocity: this.velocity,
-        }));
+    public next(direction: number | null) {
+        if (direction !== null) {
+            this.moveNote(direction);
+            this.notes.push(Note.create({
+                note: this.note,
+                index: this.index,
+                duration: this.speed,
+                velocity: this.velocity,
+            }));
+        }
         this.index += this.speed;
         return this;
     }
